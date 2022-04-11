1950af018aaf931ab8aded40eab3d3dc6d4b8fec
Gabriel <gdbianchii@gmail.com>
2022-04-11 14:16:01 -0300

    Add project files.

---
+++ BellunoApp/BellunoApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BellunoApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BellunoApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ BellunoApp/BellunoApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using BellunoApp.Models;
+using BellunoApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BellunoApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
